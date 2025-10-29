222ce7f26a8bc0046eb5600ac9fb0c4376d9a811
pizzaman <pizzaman3849@gmail.com>
2025-10-29 21:08:06 +0900

    feat: 맵 출력, 게임 종료 기능 구현, 전투 함수 일부 구현
    맵 출력 함수의 이름을 printMap에서 getMap으로 변경하고 배열 동적 할당 및 초기화 후 주소 반환하는 기능 추가. gameOver, gameClear 함수에서 메시지 출력 후 파일 종료 기능 구현.
    전투 함수의 구조를 플레이어와 적의 행동을 번갈아 받는 무한루프 구조로 구현함(상세한 기능 구현X)

--- src/GameManager.cpp
+++ src/GameManager.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include <cstdlib>
 #include <random>
+#include <chrono>
+#include <thread>
 #include "../include/rpg.h"
 
-void printmap(int *playerLocation, int *exit) {
+int* getMap() {
+    int* arr = new int[5 * 5]; // 동적 메모리
+    for (int i = 0; i < 25; i++)
+        arr[i] = 0; // 예시로 값 초기화
+    return arr;     // OK (스택에서 안 사라짐)
+}
+void printmap(int playerLocation, int enemyLocation, int *exit) {
     for (int i = 0; i < 5; ++i) {
         for (int j = 0; j < 5; ++j) {
             std:: cout << "[";
@@ -37,14 +45,44 @@ int getDice() {
         return false;
     }
  }
-    void battle() {
-        std::cout<< "전투 시작" << endl;
+    void battle(Player &refPlayer, Enemy &refEnemy) {
+        std::cout<< "전투 시작" << std::endl;
         while(1) {
-            getPlayerAction();
-            if()
-            getEnemyAction();
+            if (getPlayerAction() == 1) {
+                refPlayer.attack(int refPlayer.atk, int refEnemy.hp)
+            }
+            else if(getPlayerAction() == 2) {
+                refPlayer.defend(int refPlayer.def, int refEnemy.hp);
+            }
+            else if(getPlayerAction() == 3) {
+                
+            }
+
+            if (getEnemyAction() == 1) {
+
+            }
+            else if(getEnemyAction() == 2) {
+
+            }
+            if (refPlayer.hp == 0) {
+                gameOver();
+            }
 
         }
     }
-    void gameOver();
-    void gameClear();
+    void gameOver() {
+        std::cout << "Game Over";
+        for (int i = 5; i > 0; --i) {
+            std::cout << i << "초 후 게임 종료" << std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+        exit(1);
+    }
+    void gameClear() {
+        std::cout << "Game Clear!";
+        for (int i = 5; i > 0; --i) {
+            std::cout << i << "초 후 게임 종료" << std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+        }
+        exit(1);
+    }
