918af8f4657ed468915b3b68498babfaa0d35a4f
pizzaman <pizzaman3849@gmail.com>
2025-11-04 00:37:55 +0900

    chore: 헤더파일 수정
    GameManager 브랜치에서 완성한 헤더파일로 덮어씀.

--- include/rpg.h
+++ include/rpg.h
@@ -1,32 +1,38 @@
 #pragma once//include 시 #include "../include/rpg.h" 사용할 것(상대경로)
-class GameManager {
-    public: 
-    int map[5][5];//맵 배열 생성
-    void printMap(int map);//맵 출력 함수
-    int getExit(int map[5][5]);//배열 인덱스의 주소 반환
-    int getDice();//주사위 함수
-    bool ifBattleStart();
-    void battle();
-    void gameOver();
-    void gameClear();
-};
+
 class Character {
     int *location;//배열 인덱스의 주소 
     int hp;//05 고정
     int atk;//10 고정
     int def;//100 고정
-    int attack(int atk, int hp);//hp = 상대방 체력
-    int defend(int def, int hp);//hp = 상대방 체력
+    public:
+    int getHp() {return this->hp;}//hp 리턴
+    int setHp(int atk = 10, int def = 100);//hp 수정 후 리턴
 
 };
-class Player : Character {
-    int fitstLocation();
+class Player : public Character {
+    public:
     int getLocation(int *location);//배열 인덱스의 주소 반환
     int move(int *location);//배열 인덱스의 주소 반환
-    void getPlayerAction(); //플레이어 행동 선택
+    int getPlayerAction(); //플레이어 행동 선택
+    int attack(Enemy& refEnemy);
+    int defend(Enemy& refEnemy);
     void run();
 };
-class Enemy : Character {
-    int makeLocaiotn ();//배열 인덱스의 주소 반환
-    void getEnemyAction(); //적 행동 무작위 결정
+class Enemy : public Character {
+    public:
+    int getEnemyAction(); //적 행동 무작위 결정
+    int attack(Player& refPlayer);
+    int defend(Player& refPlayer);
 };
+namespace GameManager {
+    int* getMap();//맵 생성 함수
+    int* getStart(int (*map)[5][5]);
+    int* getExit(int (*map)[5][5]);
+    void printMap(int (*map)[5][5]);//맵 출력 함수
+    int getDice();//주사위 함수
+    bool ifBattleStart(int playerLocation, int enemyLocation);
+    void battle(Player &refPlayer, Enemy &refEnemy);
+    void gameOver();
+    void gameClear();
+}
