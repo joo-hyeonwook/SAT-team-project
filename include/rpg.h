6783cb1f1878e88237e5e8ae0dae2cba96adf4d1
pizzaman <pizzaman3849@gmail.com>
2025-11-03 22:49:35 +0900

    chore: 헤더파일 수정
    Character 클래스의 생성자에서 위치를 생성함에 따라 위치 생성 함수를 삭제함. 전투 함수에 필요한 메서드 추가.

--- include/rpg.h
+++ include/rpg.h
@@ -1,19 +1,18 @@
 #pragma once//include 시 #include "../include/rpg.h" 사용할 것(상대경로)
 
 class Character {
-    int location;//배열 인덱스의 주소 (map[i][j]라면 location == i*10+j)
+    int *location;//배열 인덱스의 주소 (map[i][j]라면 location == i*10+j)
     int hp;//05 고정
     int atk;//10 고정
     int def;//100 고정
-    public:
-    int attack(int myAtk, int opponentHp);//hp = 상대방 체력
-    int defend(int myDef, int opponentHp);//hp = 상대방 체력
+public:
+    void setHp(int atk = 10, int def = 100);// 체력 재설정
+    int getHp();//체력 받기용 함수
 
 
 };
 class Player : Character {
     public:
-    int fitstLocation();
     int getLocation(int *location);//배열 인덱스의 주소 반환
     int move(int *location);//배열 인덱스의 주소 반환
     int getPlayerAction(); 
@@ -21,15 +20,15 @@ class Player : Character {
     void run();
 };
 class Enemy : Character {
-    public:
-    int makeLocaiotn ();//배열 인덱스의 주소 반환
+public:
     int getEnemyAction(); //적 행동 무작위 결정 후 리턴(공격1, 방어2)
 };
+
 namespace GameManager {
     int* getMap();//맵 생성 함수
-    int* getExit(int map);
-    void printMap(int map);//맵 출력 함수
-    int getExit(int map[5][5]);//배열 인덱스의 주소 반환
+    int* getStart(int (*map)[5][5]);
+    int* getExit(int (*map)[5][5]);
+    void printMap(int (*map)[5][5]);//맵 출력 함수
     int getDice();//주사위 함수
     bool ifBattleStart(int playerLocation, int enemyLocation);
     void battle(Player &refPlayer, Enemy &refEnemy);
