811d048bf5e8c9caa96dba42ee3dbc0dc30ab59f
pizzaman <pizzaman3849@gmail.com>
2025-11-06 14:28:46 +0900

    chore: 메서드 함수 선언수정
    메서드 함수의 타입과 이름, 매개변수의 타입을 전투 함수와 호환되도록 변경함.

--- include/rpg.h
+++ include/rpg.h
@@ -1,31 +1,33 @@
 #pragma once//include 시 #include "../include/rpg.h" 사용할 것(상대경로)
 
 class Character {
+    public:
     int *location;//배열 인덱스의 주소 
     int hp;//05 고정
     int atk;//10 고정
     int def;//100 고정
-    public:
+    Character();
     int getHp() {return this->hp;}//hp 리턴
-    int setHp(int atk = 10, int def = 100);//hp 수정 후 리턴
+    void setHp(int atk = 10, int def = 100);//hp 수정 후 리턴
 
 };
 class Player : public Character {
     public:
-    int getLocation(int *location);//배열 인덱스의 주소 반환
+    int* getLocation();//배열 인덱스의 주소 반환
     int move(int *location);//배열 인덱스의 주소 반환
     int getPlayerAction(); //플레이어 행동 선택
-    int attack(Enemy& refEnemy);
-    int defend(Enemy& refEnemy);
+    int playerAttack(Enemy& refEnemy, int enemyDef);
+    int playerDefend();
     void run();
 };
 class Enemy : public Character {
     public:
     int getEnemyAction(); //적 행동 무작위 결정
-    int attack(Player& refPlayer);
-    int defend(Player& refPlayer);
+    int enemyAttack(Player& refPlayer, int playerDef);
+    int enemyDefend();
 };
 namespace GameManager {
+    void printTutorial();
     int* getMap();//맵 생성 함수
     int* getStart(int (*map)[5][5]);
     int* getExit(int (*map)[5][5]);
